fa3bbe7f37470adf7db3f4273dbc92a670625771
radhi0 <109933065+radhi0@users.noreply.github.com>
2022-10-14 12:47:19 +1030

    Update Player.h

--- Player.h
+++ Player.h
@@ -10,24 +10,26 @@ using namespace std;
 class Player {
 protected:
     string name;
-    PhysicalAttributes::PhysicalAttributes() physicalAttributes_rates;
+    PhysicalAttributes physicalAttributes_rates;
     CommonSkills commonSkills_rates;
     float requiredSalary_for_player;
     float printSalary_of_selected_player;
     
 public:
     Player();
-    Player(string _name, PhysicalAttributes _physicalAttributes_rates, CommonSkills _commonSkill_rates, float salary);
+    Player(string _name, PhysicalAttributes _physicalAttributes_rates, CommonSkills _commonSkill_rates, float _salary);
     void set_name(string _name);
     void set_physicalAttribute_rates(PhysicalAttributes _physicalAttribute_rates);
-    void set_commonSkills_rates(CommonSkills _common_skill_rates);
-    void set_requiredSalary_for_player(float _required_player_salary);
+    void set_commonSkills_rates(CommonSkills _commonSkill_rates);
+    void set_requiredSalary_for_player(float _requiredSalary_for_player);
+    void set_printSalary_of_selected_player(float _printSalary_of_selected_player);
     string get_name();
     PhysicalAttributes get_physicalAttributes_rates();
     CommonSkills get_commonSkill_rates();
     float get_requiredSalary_for_players() const;
+    float get_printSalary_of_selected_player() const;
     virtual void printing_info();
-    virtual float total_player_rates() = 0;
+    virtual float totalPlayer_rates() = 0;
     ~Player();
 };
 
