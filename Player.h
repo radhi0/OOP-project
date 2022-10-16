37d445b9315b3fa281455556139063b9a1649e87
ahardecoding <109941686+ahardecoding@users.noreply.github.com>
2022-10-16 10:56:17 +1030

    Update Player.h

--- Player.h
+++ Player.h
@@ -18,11 +18,13 @@ protected:
 public:
     Player();
     Player(string _name, PhysicalAttributes _physicalAttributes_rates, CommonSkills _commonSkill_rates, float _salary);
+    //setters
     void set_name(string _name);
     void set_physicalAttribute_rates(PhysicalAttributes _physicalAttribute_rates);
     void set_commonSkills_rates(CommonSkills _commonSkill_rates);
     void set_requiredSalary_for_player(float _requiredSalary_for_player);
     void set_printSalary_of_selected_player(float _printSalary_of_selected_player);
+    //getters
     string get_name();
     PhysicalAttributes get_physicalAttributes_rates();
     CommonSkills get_commonSkill_rates();
