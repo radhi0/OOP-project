b8a5b51237a228e5c121242b7802356358a76a8f
ahardecoding <109941686+ahardecoding@users.noreply.github.com>
2022-10-16 11:18:40 +1030

    Update PointGuard.h

--- PointGuard.h
+++ PointGuard.h
@@ -13,9 +13,11 @@ public:
     PointGuard();
     PointGuard(string _name, PhysicalAttributes _physicalAttributes_rates, 
     CommonSkills _commonSkills_rates, float _salary, Attribute Shooting, Attribute Accuracy, Attribute freeThrows);
+    //setters
     void set_shooting(Attribute _Shooting);
     void set_accuracy(Attribute _Accuracy);
     void set_freeThrows(Attribute _freeThrows);
+    //getters
     Attribute get_shooting();
     Attribute get_accuracy();
     Attribute get_freeThrows();
