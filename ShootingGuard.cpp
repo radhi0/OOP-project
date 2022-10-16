48db417dfcfeaea8143b650f7fde531976f3def9
ahardecoding <109941686+ahardecoding@users.noreply.github.com>
2022-10-16 11:55:04 +1030

    Update ShootingGuard.cpp

--- ShootingGuard.cpp
+++ ShootingGuard.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 ShootingGuard::ShootingGuard(){}
-
+//renaming the variable names
 ShootingGuard::ShootingGuard(string _name, PhysicalAttributes _physicalAttributes_rates, 
     CommonSkills _commonSkills_rates, float _salary, Attribute _Shooting, Attribute _Accuracy, Attribute _freeThrows){
         name = _name;
@@ -15,13 +15,14 @@ ShootingGuard::ShootingGuard(string _name, PhysicalAttributes _physicalAttribute
         Accuracy = _Accuracy;
         freeThrows = _freeThrows;
 }
-    
+//setters    
 void ShootingGuard::set_shooting(Attribute _Shooting){Shooting = _Shooting;}
 
 void ShootingGuard::set_accuracy(Attribute _Accuracy){Accuracy = _Accuracy;}
 
 void ShootingGuard::set_freeThrows(Attribute _freeThrows){freeThrows = _freeThrows;}
 
+//getters
 Attribute ShootingGuard::get_shooting(){return Shooting;}
 
 Attribute ShootingGuard::get_accuracy(){return Accuracy;}
