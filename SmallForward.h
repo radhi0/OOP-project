1d3d5d570740d4be43bbb295ac566920fd823e1d
ahardecoding <109941686+ahardecoding@users.noreply.github.com>
2022-10-16 12:12:19 +1030

    Update SmallForward.h

--- SmallForward.h
+++ SmallForward.h
@@ -16,6 +16,7 @@ class SmallForward : public Player {
   SmallForward(string _name, PhysicalAttributes _physicalAttributes_rates,
                CommonSkills _commonSkills_rates, float _salary,
                Attribute _Shooting, Attribute _Accuracy, Attribute _freeThrows);
+  //setter
   void set_shooting(Attribute _Shooting);
   void set_accuracy(Attribute _Accuracy);
   void set_freeThrows(Attribute _freeThrows);
