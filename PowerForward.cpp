13eccb16c08d0235e5bf10b994cd6b6fa788dfd9
ahardecoding <109941686+ahardecoding@users.noreply.github.com>
2022-10-16 11:44:23 +1030

    Update PowerForward.cpp

--- PowerForward.cpp
+++ PowerForward.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 PowerForward::PowerForward(){}
-
+//renaming the variables
 PowerForward::PowerForward(string _name, PhysicalAttributes _physicalAttributes_rates, 
     CommonSkills _commonSkills_rates, float _salary, Attribute _Shooting, Attribute _Accuracy, Attribute _freeThrows){
         name = _name;
@@ -15,13 +15,13 @@ PowerForward::PowerForward(string _name, PhysicalAttributes _physicalAttributes_
         Accuracy = _Accuracy;
         freeThrows = _freeThrows;
 }
-
+//setters
 void PowerForward::set_shooting(Attribute _Shooting){Shooting = _Shooting;}
 
 void PowerForward::set_accuracy(Attribute _Accuracy){Accuracy = _Accuracy;}
 
 void PowerForward::set_freeThrows(Attribute _freeThrows){freeThrows = _freeThrows;}
-
+//getters
 Attribute PowerForward::get_shooting(){return Shooting;}
 
 Attribute PowerForward::get_accuracy(){return Accuracy;}
@@ -34,7 +34,7 @@ void PowerForward::printing_info() {
     cout << "|| Accuracy: " << Accuracy.get_rates() << " ";
     cout << "|| Free Throws: " << freeThrows.get_rates() << endl;
 }
-
+//return
 float PowerForward::totalPlayer_rates() {
     return (Player::totalPlayer_rates() + (float(Shooting.get_rates()) + float(Accuracy.get_rates()) + float(freeThrows.get_rates()))/3)/2;
 }
